c5c1f7aa84ad48ff9827db9030e8dcf35d2456d0
afifassihab <afifassihab@gmail.com>
2021-03-09 20:01:39 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let textEditor = document.querySelector("#text-editor");
+const boldBtn = document.querySelector("#bold-btn");
+
+boldBtn.addEventListener("click", function () {
+    // alert(window.getSelection());
+    // alert(document.getSelection())
+    console.log(window)
+})
